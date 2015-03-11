d8aee90d0369ee4204b83efff5f7ed80859f8156
light94 <mishra.rahul1712@gmail.com>
2015-03-12 02:12:34 +0530

    created dictionary for atom metadata

--- parser.py
+++ parser.py
@@ -19,9 +19,30 @@ for i,j,k in expression.scanString(data):
 
 #now see if .gam file regarding each atom is present or not.
 
-dataFile = []
+#dataFile = []
 filename = Word(alphanums)+".gam"
 for url in atomUrlList:
 	fileListing = urlopen(url).read()
 	generator = filename.scanString(fileListing)
-	dataFile.append(next(generator,None))
+	#dataFile.append(next(generator,None))
+	data = urlopen(next(generator,None)).read()
+	create_dictionary(data)
+
+
+def create_dictionary(data):
+	
+
+	#read atom detail which is 5 characters long
+	atom = data.read(5)
+
+	dictionary = {}
+	dictionary['atom'] = atom
+
+	label = delimitedList(Word(alphas+'-'+nums), delim=' ', combine=True)
+	line = Word(nums) + label
+
+	#create dictionary to store observation metadata for atom = 'atom'
+	for line_tokens, start_location, end_location in line.scanString(data.readline()):
+		dictionary[line_tokens[1]] = line_tokens[0]	
+
+	print dictionary
