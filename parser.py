ae07f8125d26120fd3e0be788a4e5a715122c44a
light94 <mishra.rahul1712@gmail.com>
2015-03-12 02:03:17 +0530

    fetching .gam files enabled

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-from pyparsing import Word, alphanums
+from pyparsing import Word, alphanums,alphas
 from urllib2 import urlopen
 
 expression = 'HREF="'+Word(alphanums+':/.')+'"'
@@ -14,6 +14,14 @@ data = atomData.read()
 for i,j,k in expression.scanString(data):
 	atomUrlList.append(i[1])
 
-print atomUrlList
+#print atomUrlList
+# atomUrlList = ['http://kurucz.harvard.edu/atoms/0400/']
 
+#now see if .gam file regarding each atom is present or not.
 
+dataFile = []
+filename = Word(alphanums)+".gam"
+for url in atomUrlList:
+	fileListing = urlopen(url).read()
+	generator = filename.scanString(fileListing)
+	dataFile.append(next(generator,None))
