f35caab6249f0ebc655a23c3fa4aebd8d5d4dece
light94 <mishra.rahul1712@gmail.com>
2015-03-18 14:52:00 +0530

    curl utility for NIST data

---
+++ parserNist.sh
@@ -0,0 +1,1 @@
+curl --data  "encodedlist=XXT2&spectrum=Se%2b&submit=Retrieve%20Data&units=0&format=0&output=0&page_size=15&multiplet_ordered=0&conf_out=on&term_out=on&level_out=on&j_out=on&lande_out=on" -o "data.txt" http://physics.nist.gov/cgi-bin/ASD/energy1.pl
